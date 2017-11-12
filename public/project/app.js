95cfd5afa720df2321e28ec5fa8b7678d551feb1
maloomis <loomis.m@husky.neu.edu>
2017-11-12 17:56:10 -0500

    carousel

--- public/project/app.js
+++ public/project/app.js
@@ -1,3 +1,3 @@
 (function () {
-        angular.module('FreshmanApp', ['ngRoute','ngAnimate', 'ngSanitize', 'ui.bootstrap', 'angularjs-dropdown-multiselect' ]);
+        angular.module('FreshmanApp', ['ngRoute','ngAnimate', 'ngSanitize', 'ui.bootstrap', 'angularjs-dropdown-multiselect', 'ui.carousel']);
 })();
